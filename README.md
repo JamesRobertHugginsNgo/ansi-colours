d15472262a18eece7b5abb31525f314e64eed758
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2022-12-20 23:36:44 -0500

    Add main and test code

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # ansi-colours
+
+Version 1.0.0
+
+Lists ANSI escape code for displaying colours on console log.
+
+## Constants
+
+- RESET
+- BLACK
+- RED
+- GREEN
+- YELLOW
+- BLUE
+- MAGENTA
+- CYAN
+- WHITE
+- B_BLACK
+- B_RED
+- B_GREEN
+- B_YELLOW
+- B_BLUE
+- B_MAGENTA
+- B_CYAN
+- B_WHITE
+- BG_BLACK
+- BG_RED
+- BG_GREEN
+- BG_YELLOW
+- BG_BLUE
+- BG_MAGENTA
+- BG_CYAN
+- BG_WHITE
+- BG_B_BLACK
+- BG_B_RED
+- BG_B_GREEN
+- BG_B_YELLOW
+- BG_B_BLUE
+- BG_B_MAGENTA
+- BG_B_CYAN
+- BG_B_WHITE
+
+## Reference
+
+https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
---
+++ index.js
@@ -0,0 +1,41 @@
+const ANSI_COLOURS = {
+	RESET: '\u001b[0m',
+
+	BLACK: '\u001b[30m',
+	RED: '\u001b[31m',
+	GREEN: '\u001b[32m',
+	YELLOW: '\u001b[33m',
+	BLUE: '\u001b[34m',
+	MAGENTA: '\u001b[35m',
+	CYAN: '\u001b[36m',
+	WHITE: '\u001b[37m',
+
+	B_BLACK: '\u001b[30;1m',
+	B_RED: '\u001b[31;1m',
+	B_GREEN: '\u001b[32;1m',
+	B_YELLOW: '\u001b[33;1m',
+	B_BLUE: '\u001b[34;1m',
+	B_MAGENTA: '\u001b[35;1m',
+	B_CYAN: '\u001b[36;1m',
+	B_WHITE: '\u001b[37;1m',
+
+	BG_BLACK: '\u001b[40m',
+	BG_RED: '\u001b[41m',
+	BG_GREEN: '\u001b[42m',
+	BG_YELLOW: '\u001b[43m',
+	BG_BLUE: '\u001b[44m',
+	BG_MAGENTA: '\u001b[45m',
+	BG_CYAN: '\u001b[46m',
+	BG_WHITE: '\u001b[47m',
+
+	BG_B_BLACK: '\u001b[40;1m',
+	BG_B_RED: '\u001b[41;1m',
+	BG_B_GREEN: '\u001b[42;1m',
+	BG_B_YELLOW: '\u001b[43;1m',
+	BG_B_BLUE: '\u001b[44;1m',
+	BG_B_MAGENTA: '\u001b[45;1m',
+	BG_B_CYAN: '\u001b[46;1m',
+	BG_B_WHITE: '\u001b[47;1m'
+};
+
+module.exports = ANSI_COLOURS;
